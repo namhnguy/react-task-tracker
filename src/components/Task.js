fd51a980da00abb09fce325fcd966ae91c873ede
namhnguy <namhnguy@gmail.com>
2021-10-03 20:05:50 -0500

    initial

---
+++ src/components/Task.js
@@ -0,0 +1,16 @@
+import {faTimes} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+const Task = ({task, onDelete, onToggle}) => {
+    return (
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+            <h3>
+                {task.text} <FontAwesomeIcon icon={faTimes} style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)} />
+            </h3>
+            <p>{task.day}</p>
+        </div>
+    )
+}
+
+export default Task
